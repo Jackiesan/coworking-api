6d4cc8e90174a1f380ec7212504cb6081ad89270
Jackie Onofre <jackie.onofre@tylertech.com>
2019-07-22 20:52:06 -0700

    Add route to return ist of employees and delete of company for a unit

--- api/routes/units.js
+++ api/routes/units.js
@@ -2,10 +2,28 @@ const router = require('express').Router()
 const Units = require('../models/units')
 var mongoose = require('mongoose');
 
+// GET /api/v1/units
+// GET /api/v1/units?kind=[kind]
+// GET /api/v1/units?floor=[integer]
+// TODO: GET /api/v1/units?occupied=[true/false]
 router.get('/', async (req, res, next) => {
   status = 200
   const response = await Units.find(({...req.query}))
   res.json({ status, response })
+})
+
+// GET /api/v1/units/[id]/company/employees
+router.get('/:id/company/employees', async (req, res, next) => {
+  status = 200
+  try {
+    const unit = await Units.findById(req.params.id)
+    const employees = unit.company.employees
+    res.status(status).json({ status, employees })
+  } catch (error) {
+    const e = new Error(`Unit with ID ${req.params.id} not found`)
+    e.status = 400
+    next(e)
+  }
 })
 
 router.post('/', async (req, res, next) => {
@@ -61,8 +79,22 @@ router.patch('/:id', async (req, res, next) => {
   }
 })
 
-// PATCH /api/v1/units/[id]/company
+// DELETE http://localhost:5000/api/v1/units/5/company
 
+router.delete('/:id/company', async (req, res, next) => {
+  const status = 200
+
+  try{
+    const unit = await Units.findById(req.params.id)
+    unit.company.remove()
+    await unit.save()
+    res.status(status).json({ status, unit })
+  } catch (error) {
+    const e = new Error(`Unit with ID ${req.params.id} not found`)
+    e.status = 404
+    next(e)
+  }
+})
 
 
 module.exports = router
